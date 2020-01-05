f24c8d2a3ef521cf8700b1b7bc5c15aa734f12eb
jpm <jpmckenna90@gmail.com>
2020-01-04 19:25:32 -0500

    Cleaned up content progression logic. Improved responsiveness across device sizes.

--- script.js
+++ script.js
@@ -1,60 +1,121 @@
 
-// var settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer?q=How%20much%20vitamin%20c%20is%20in%202%20apples%253F",
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-// 		"x-rapidapi-key": "a24fa84bbda24ba5a81304ccf4121858"
-// 	}
-// }
+        // var searchedItems = [];
 
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
+        // var inputItem;
+        // $("#inputBtn").on("click", function (event) {
+        //     event.preventDefault();
+            /*
+            clearField();
+            */
+            // console.log(event.target);
+            // var inputItem = $("#inputSearch").val().trim().toLowerCase();
+            //console.log(inputItem);
+            // searchedItems.unshift(inputItem);
+            // console.log(searchedItems);
+            // Ajax();
+            //AJAX2();
+        // });
 
-// $.ajax({
-//   url: "https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=a24fa84bbda24ba5a81304ccf4121858",
-//   method: "GET"
-// }).then(function(response){
-//   console.log(response);
-// })
+        // function Ajax() {
+        //     var inputItem = $("#inputSearch").val().trim().toLowerCase();
+        //     var ingredientDomainName = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
+        //     var queryUrl = ingredientDomainName + searchedItems[0] + "instructionsRequired=true" + "&number=5";
+        //     var apiKey = "256cd3ee2e0548e59e4990ad44a8ec31";
+            /*
+             var queryUrl = ingredientDomainName + "chicken" + "instructionsRequired=true" + "&number=10";
+             //"apples,+flour,+sugar" 
+            */
+        /*
+            /*
+            var apiKey = "a24fa84bbda24ba5a81304ccf4121858";
+            */
+      
+        //     $.ajax({
+        //         url: queryUrl + "&apiKey=" + apiKey,
+        //         method: "GET"
+        //     }).then(function (response) {
+        //         console.log(response);
+        //         for (var i = 0; ; i++) {
+        //             console.log(response[i].title);
+        //         }
+        //     });
+        // }
 
-// var queryUrl = "https://api.spoonacular.com/food/products/search?query=yogurt"
-// var apiKey = "a24fa84bbda24ba5a81304ccf4121858";
+ /*
+        function AJAX2() {
+            var mealPlanDomainName = "https://api.spoonacular.com/recipes/mealplans/generate";
+            var mealPlanUrl = "https://api.spoonacular.com/recipes/mealplans/generate?timeFrame=day&diet=paleo";
+            var apiKey = "256cd3ee2e0548e59e4990ad44a8ec31";
 
-// $.ajax({
-//   url: queryUrl + "&apiKey=" + apiKey,
-//   method: "GET"
-// }).then(function(response){
-//   console.log(response);
-// })
+            $.ajax({
+                url: mealPlanUrl + "&apiKey=" + apiKey,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+            });
+        }
+// */
+//         function clearField() {
+//             $("#results").empty();
+//         }
+//         $("#clear").on("click", clearField);
 
-$("document").ready(function() {            
+// Testing a variable to hold the current page user is on - could (should) probably be done with strings to be clear 
+var currentPage = 0;
 
-  $( "#startbutton" ).click(function() {
-    // Scroll landing page out of the way
+    
+$("document").ready(function() {     
+//   Function to move forward from initial landing page 
+  function moveLanding(){
     $("#landing").animate({
       opacity: 0,
       bottom: "+=900",
-    }, 600 );
-    // Reveal options page and scroll into view
+    }, 600);
     $("#options").show();
     $("#options").animate({
-      bottom: "+=700",
+      opacity: 1,
+      bottom: "+=400",
     }, 600);
+    currentPage++;
+  }
+//   Function to step back in page content
+  function moveBack(){
+    if(currentPage == 1){
+        $("#options").animate({
+            opacity: 0,
+            bottom: "-=400",
+          }, 600);
+          $("#landing").animate({
+            opacity: 1,
+            bottom: "-=900",
+          }, 600);
+          currentPage--;
+    }
+  }
+    // Function to progress page content
+  function moveForward(){
+      if(currentPage == 1){
+        $("#options").animate({
+            opacity: 0,
+            bottom: "+=400",
+          }, 600);
+          currentPage++;
+      }
+  }
+    //   Page movement event handlers
+   $("#startbutton").on("click", function(){
+        moveLanding();
     })
+    $("#backbutton").click(function(){
+        moveBack();
+  })
+    $("#nextbutton").click(function(){
+        moveForward();
+    })
+})
 
-    $( "#nextbutton" ).click(function() {
-      // Scroll landing page out of the way
-      // Reveal options page and scroll into view
-      $("#options").animate({
-        opacity: 0,
-        bottom: "+=700",
-      }, 600);
-      })
-  });
+
+  
 
 var intolerances = "";
 
@@ -94,7 +155,7 @@ function checkIntolerances() {
         console.log("..." + intolerances);
     });
 }
-$("#search").on("click", function() {
+$("#nextbutton").on("click", function() {
     checkIntolerances();
     var queryUrl = "https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert";
     var apiKey = "a24fa84bbda24ba5a81304ccf4121858";
@@ -103,7 +164,7 @@ $("#search").on("click", function() {
         url: queryUrl + "&apiKey=" + apiKey,
         method: "GET"
     }).then(function(response) {
-        //console.log(response.recipes[0]);
+        console.log(response.recipes[0]);
     });
 });
 
