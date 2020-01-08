17d047780054227fd45b5ac058fdb9715180f733
dchen <chendana95@gmail.com>
2020-01-07 20:57:00 -0500

    Forloop checkboxes (#6)
    * updated js with for loop to add value of checkboxes but not remove when unchecked
    
    * changed API queryURL but still does not work with more than one input
    
    * added comma separators to intolerances
    
    * fixed comma separators...

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
+var searchedItems = [];
 
-        var searchedItems = [];
+var inputItem;
+$("#inputBtn").on("click", function(event) {
+    event.preventDefault();
+    /*
+    clearField();
+    */
+    console.log(event.target);
+    var inputItem = $("#inputSearch").val().trim().toLowerCase();
+    //console.log(inputItem);
+    searchedItems.unshift(inputItem);
+    console.log(searchedItems);
+    Ajax();
+    //AJAX2();
+});
 
-        var inputItem;
-        $("#inputBtn").on("click", function (event) {
-            event.preventDefault();
-            /*
-            clearField();
-            */
-            console.log(event.target);
-            var inputItem = $("#inputSearch").val().trim().toLowerCase();
-            //console.log(inputItem);
-            searchedItems.unshift(inputItem);
-            console.log(searchedItems);
-            Ajax();
-            //AJAX2();
-        });
-
-        function Ajax() {
-            var inputItem = $("#inputSearch").val().trim().toLowerCase();
-            var ingredientDomainName = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
-            var queryUrl = ingredientDomainName + searchedItems[0] + "instructionsRequired=true" + "&number=5";
-            var apiKey = "256cd3ee2e0548e59e4990ad44a8ec31";
-            /*
-             var queryUrl = ingredientDomainName + "chicken" + "instructionsRequired=true" + "&number=10";
-             //"apples,+flour,+sugar" 
-            */
+function Ajax() {
+    var inputItem = $("#inputSearch").val().trim().toLowerCase();
+    var ingredientDomainName = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
+    var queryUrl = ingredientDomainName + searchedItems[0] + "instructionsRequired=true" + "&number=5";
+    var apiKey = "256cd3ee2e0548e59e4990ad44a8ec31";
+    /*
+     var queryUrl = ingredientDomainName + "chicken" + "instructionsRequired=true" + "&number=10";
+     //"apples,+flour,+sugar" 
+    */
+    /*
         /*
-            /*
-            var apiKey = "a24fa84bbda24ba5a81304ccf4121858";
-            */
-      
-            $.ajax({
-                url: queryUrl + "&apiKey=" + apiKey,
-                method: "GET"
-            }).then(function (response) {
-                console.log(response);
-                for (var i = 0; ; i++) {
-                    console.log(response[i].title);
-                }
-            });
-        }
+        var apiKey = "a24fa84bbda24ba5a81304ccf4121858";
+        */
 
- /*
+    $.ajax({
+        url: queryUrl + "&apiKey=" + apiKey,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        for (var i = 0;; i++) {
+            console.log(response[i].title);
+        }
+    });
+}
+
+/*
         function AJAX2() {
             var mealPlanDomainName = "https://api.spoonacular.com/recipes/mealplans/generate";
             var mealPlanUrl = "https://api.spoonacular.com/recipes/mealplans/generate?timeFrame=day&diet=paleo";
@@ -55,97 +54,58 @@
             });
         }
 */
-        function clearField() {
-            $("#results").empty();
-        }
-        $("#clear").on("click", clearField);
+function clearField() {
+    $("#results").empty();
+}
+$("#clear").on("click", clearField);
 
-// var settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer?q=How%20much%20vitamin%20c%20is%20in%202%20apples%253F",
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-// 		"x-rapidapi-key": "a24fa84bbda24ba5a81304ccf4121858"
-// 	}
-// }
 
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
 
-// $.ajax({
-//   url: "https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=a24fa84bbda24ba5a81304ccf4121858",
-//   method: "GET"
-// }).then(function(response){
-//   console.log(response);
-// })
+$("document").ready(function() {
 
-// var queryUrl = "https://api.spoonacular.com/food/products/search?query=yogurt"
-// var apiKey = "a24fa84bbda24ba5a81304ccf4121858";
-
-// $.ajax({
-//   url: queryUrl + "&apiKey=" + apiKey,
-//   method: "GET"
-// }).then(function(response){
-//   console.log(response);
-// })
-
-$("document").ready(function() {            
-
-  $( "#button" ).click(function() {
-    // Scroll landing page out of the way
-    $("#landing").animate({
-      bottom: "+=900",
-    }, 600 );
-    // Reveal options page and scroll into view
-    $("#options").show();
-    $("#options").animate({
-      bottom: "+=800",
-    }, 600);
+    $("#button").click(function() {
+        // Scroll landing page out of the way
+        $("#landing").animate({
+            bottom: "+=900",
+        }, 600);
+        // Reveal options page and scroll into view
+        $("#options").show();
+        $("#options").animate({
+            bottom: "+=800",
+        }, 600);
     })
-  });
+});
+
+
 
 var intolerances = "";
+var updatedIntolerances = "";
+checkIntolerances();
 
 function checkIntolerances() {
-    event.preventDefault();
-    $('input[type="checkbox"]').on('change', function() {
-        var shellfish = $('#check0').prop('checked');
-        var gluten = $('#check1').prop('checked');
-        var dairy = $('#check2').prop('checked');
-        var peanuts = $('#check3').prop('checked');
-        //$("#inputSearch").val()
-        //var checkMe = [shellfish, gluten, dairy, peanuts];
-        // for (var i = 0; i < 5; i++) {
-        // console.log(checkMe[i]);
-        // }
-        if (!shellfish && !gluten && !dairy && !peanuts) {
-            intolerances = '';
-        } else if (shellfish && gluten && dairy && peanuts) {
-            intolerances = "shellfish,eggs,gluten,dairy,peanuts";
-        } else if ((shellfish && gluten && dairy) && !peanuts) {
-            intolerances = "shellfish,gluten,dairy";
-        } else if ((shellfish && gluten) && !dairy && !peanuts) {
-            intolerances = "shellfish,gluten";
-        } else if (shellfish) {
-            intolerances = "shellfish"
-        } else if (gluten) {
-            intolerances = "gluten";
-        } else if (dairy) {
-            intolerances = "dairy";
-        } else if (peanuts) {
-            intolerances = "peanuts";
-        } else if (!shellfish && gluten && dairy && !peanuts) {
-            intolerances = "gluten,dairy"
-        } else if (!shellfish && gluten && dairy && peanuts) {
-            intolerances = "gluten,dairy,peanuts"
+    var inputs = $('input[type="checkbox"]');
+    //var checkMe = $("#checkme");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type === 'checkbox') {
+            inputs[i].onclick = function() {
+                // if (checkMe.checked === true) {
+                //     console.log("ADD")
+                // } else {
+                //     console.log("REMOVE")
+                // }
+                intolerances = intolerances + this.value + ",";
+                updatedIntolerances = intolerances.slice(0, -1);
+                console.log(updatedIntolerances);
+                //return intolerances;
+                //getRecipeWithIntolerances();
+            }
         }
-        console.log("..." + intolerances);
-    });
+    }
 }
-$("#search").on("click", function() {
+
+//get random recipe
+/*
+$("#button").on("click", function() {
     checkIntolerances();
     var queryUrl = "https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert";
     var apiKey = "a24fa84bbda24ba5a81304ccf4121858";
@@ -155,6 +115,25 @@ $("#search").on("click", function() {
         method: "GET"
     }).then(function(response) {
         //console.log(response.recipes[0]);
+        var total = response.recipes[0].analyzedInstructions[0].steps.length;
+        for (var i = 0; i < total; i++) {
+            //console.log(response.recipes[0].analyzedInstructions[0].steps[i].step);
+        }
     });
-});
+});*/
 
+$("#button1").on("click", getRecipeWithIntolerances);
+
+function getRecipeWithIntolerances() {
+    var queryUrl = "https://api.spoonacular.com/recipes/search?query=pepper&number=2&intolerances=" + updatedIntolerances;
+    var apiKey = "a24fa84bbda24ba5a81304ccf4121858";
+    //var apiKey = "3ecef2433f5d402daccaccdf1550dabe";
+    $.ajax({
+        url: queryUrl + "&apiKey=" + apiKey,
+        method: "GET"
+    }).then(function(response) {
+        console.log(this.url);
+        console.log(response);
+
+    });
+}
